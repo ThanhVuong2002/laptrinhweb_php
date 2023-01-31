8d5b35b95113e34a3846e6b17fbe591fc7896186
phanthanhvuong <vuong.phan24@student.passerellesnumeriques.org>
2023-01-31 17:40:14 +0700

    Initial commit

---
+++ dientich.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<?php
+$cd=5;
+$cr=7;
+$dt=$cd+$cr;
+echo "<p>$dt</p>";
+?> 
+</body>
+</html>
---
+++ switch.php
@@ -0,0 +1,21 @@
+<html>
+    <body>
+        <?php
+        $mung= "visit the church";
+        switch($mung){
+            case "visit the church":
+                echo'Hello papa';
+                break;
+            case "visit the heo":
+                echo 'hello my mom';
+                break;
+            case"visit the run":
+                echo'hello nội';
+                break;
+            case "visit the hoo":
+                echo'hello PNV';
+                break;
+        }
+        ?>
+    </body>
+</html>
